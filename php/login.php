5b8dcc838ca0034f8104887e851497ac02b0dcc2
simaonogueira101 <simao.nogueira66@gmail.com>
2017-12-10 23:17:44 +0000

    Attempts to echo var.

--- php/login.php
+++ php/login.php
@@ -2,5 +2,6 @@
   function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["postgres://flhhfrpwgxcktk:117ff4b97a223dbef6349dd1bf86e75568d149b5ad23bcbf85d728cd7f28c6fe@ec2-79-125-118-221.eu-west-1.compute.amazonaws.com:5432/dde5e88nckdggp"]));
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+  echo $host;
   }
 ?>
