cd1531a9bccc90cc77ea30e39cf436b89b0e7413
simaonogueira101 <simao.nogueira66@gmail.com>
2017-12-10 23:15:47 +0000

    Fixes db connection issue.

--- php/login.php
+++ php/login.php
@@ -1,10 +1,6 @@
 <?php
-  $dbopts = parse_url(getenv('postgres://flhhfrpwgxcktk:117ff4b97a223dbef6349dd1bf86e75568d149b5ad23bcbf85d728cd7f28c6fe@ec2-79-125-118-221.eu-west-1.compute.amazonaws.com:5432/dde5e88nckdggp'));
-  $app->register(new Herrera\Pdo\PdoServiceProvider(),
-                 array(
-                     'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-                     'pdo.username' => $dbopts["user"],
-                     'pdo.password' => $dbopts["pass"]
-                 )
-  );
+  function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["postgres://flhhfrpwgxcktk:117ff4b97a223dbef6349dd1bf86e75568d149b5ad23bcbf85d728cd7f28c6fe@ec2-79-125-118-221.eu-west-1.compute.amazonaws.com:5432/dde5e88nckdggp"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+  }
 ?>
