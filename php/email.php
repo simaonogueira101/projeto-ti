826477c84ac557d46ae6957a446fdbd94c35e8b6
simaonogueira101 <simao.nogueira66@gmail.com>
2017-12-11 03:21:49 +0000

    more

--- php/email.php
+++ php/email.php
@@ -22,8 +22,11 @@
       $mail->SMTPDebug = 2;                                 // Enable verbose debug output
       $mail->isSMTP();                                      // Set mailer to use SMTP
       $mail->Host = "ssl://stmp.gmail.com";  // Specify main and backup SMTP servers
-      $mail->SMTPAuth = true;                               // Enable SMTP authentication
-      $mail->Username = getenv('8BitEmail');                 // SMTP username
+      $mail->SMTPAuth = true;
+
+      $myEmail = getenv('8BitEmail');
+      echo $myEmail;                          // Enable SMTP authentication
+      $mail->Username = $myEmail;                 // SMTP username
       $mail->Password = getenv('8BitPassword');                           // SMTP password
       $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
       $mail->Port = 587;                                    // TCP port to connect to
