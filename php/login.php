d5ad17c1312e5a6b6eaeb126f7bb0202fed596b8
simaonogueira101 <simao.nogueira66@gmail.com>
2017-12-11 00:59:18 +0000

    Tries to establish connection.

--- php/login.php
+++ php/login.php
@@ -1,5 +1,5 @@
 <?php
-  $dbopts = parse_url('postgres://flhhfrpwgxcktk:117ff4b97a223dbef6349dd1bf86e75568d149b5ad23bcbf85d728cd7f28c6fe@ec2-79-125-118-221.eu-west-1.compute.amazonaws.com:5432/dde5e88nckdggp');
+  $dbopts = parse_url(getenv('DATABASE_URL'));
   $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
                array(
                 'pdo.server' => array(
@@ -12,6 +12,4 @@
                    )
                )
   );
-  $query = "SELECT * FROM products;";
-  echo $query;
 ?>
